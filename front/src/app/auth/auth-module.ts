d07846d66f479566bef784c71bea9f6284e1c4f4
bloo <nexushellstein@gmail.com>
2025-11-02 15:24:20 +0800

    Added database queries

--- front/src/app/auth/auth-module.ts
+++ front/src/app/auth/auth-module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Login } from './login/login';
-import { Signup } from './signup/signup';
 import { Register } from './register/register';
-
-
 
 @NgModule({
   declarations: [
     Login,
-    Signup,
     Register
   ],
   imports: [
