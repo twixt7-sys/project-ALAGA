06390aab8dc43b1aa50891ceb7c74ca61f766892
bloo <nexushellstein@gmail.com>
2025-11-03 14:46:23 +0800

    imported components into app

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Button } from './shared/components/button/button.component';
+import { ComponentsModule } from './shared/components/components-module'
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button],
+  imports: [RouterOutlet, ComponentsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
