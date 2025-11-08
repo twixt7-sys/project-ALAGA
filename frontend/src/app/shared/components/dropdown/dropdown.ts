937d1930b72548208eb55b35528854e87dc0c776
bloo <nexushellstein@gmail.com>
2025-11-08 11:28:39 +0800

    altered dropdown component

--- frontend/src/app/shared/components/dropdown/dropdown.ts
+++ frontend/src/app/shared/components/dropdown/dropdown.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './dropdown.html',
   styleUrl: './dropdown.scss',
 })
-export class Dropdown {
-
+export class DropdownComponent {
+  @Input() label: string = '';
+  @Input() options: string[] = [];
 }
