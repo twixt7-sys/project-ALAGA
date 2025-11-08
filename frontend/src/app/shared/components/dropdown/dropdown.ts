4e969a6ff27110e9cf21398409e3ae901f51be16
bloo <nexushellstein@gmail.com>
2025-11-08 11:25:25 +0800

    created dropdown component

---
+++ frontend/src/app/shared/components/dropdown/dropdown.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  imports: [],
+  templateUrl: './dropdown.html',
+  styleUrl: './dropdown.scss',
+})
+export class Dropdown {
+
+}
