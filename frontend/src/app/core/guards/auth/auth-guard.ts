64c9c0b258b55efbede217a2f56957656880ed61
bloo <nexushellstein@gmail.com>
2025-11-14 17:51:54 +0800

    minor changes

--- frontend/src/app/core/guards/auth/auth-guard.ts
+++ frontend/src/app/core/guards/auth/auth-guard.ts
@@ -1,15 +1,26 @@
 import { CanActivateFn, Router } from '@angular/router';
 
 import { inject } from '@angular/core';
+import { AuthServices } from '../../services/auth-services';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const router = inject(Router);
-  const userId = localStorage.getItem('userId');
+  // get services
+  const auth = inject(AuthServices)
 
-  if (!userId) {
-    router.navigate(['/login']);
+  if (!auth.isLoggedIn()) return false
+
+  // guard by userId from localstorage
+  if (!localStorage.getItem('userId')) {
+    inject(Router).navigate(['/login']);
     return false;
-  } else {
-    return true;
   }
+
+  const role = auth.getRole()
+
+  const needs = route.data['role'] as string
+
+  if (!auth.isLoggedIn()) return false
+  if (needs && role !== needs) return false
+
+  return true
 };
