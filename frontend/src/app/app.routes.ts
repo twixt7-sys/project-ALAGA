2efc9ed8a19d9711f7a9fb24470b75dc058c899e
bloo <nexushellstein@gmail.com>
2025-11-14 16:36:22 +0800

    added home route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,19 +2,24 @@ import { Routes } from '@angular/router';
 import { Customer } from './features/customer/customer'
 import { Admin } from './features/admin/admin.component';
 import { Auth } from './features/auth/auth';
+import { Home } from './features/home/home';
 
 export const routes: Routes = [
     {
-        path: 'auth',
-        component: Auth
+      path: '',
+      component: Home
     },
     {
-        path: 'customer',
-        component: Customer
+      path: 'auth',
+      component: Auth
     },
     {
-        path: 'admin',
-        component: Admin
+      path: 'customer',
+      component: Customer
+    },
+    {
+      path: 'admin',
+      component: Admin
     }
 ];
 
