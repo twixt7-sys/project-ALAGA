98bcf847f007909c5c23a52c6a0b041b59c2b262
bloo <nexushellstein@gmail.com>
2025-11-14 20:48:20 +0800

    minor changes

--- frontend/src/app/core/services/index.ts
+++ frontend/src/app/core/services/index.ts
@@ -1,4 +1,4 @@
 export * from "./api/api-services"
 export * from "./auth/auth-services"
-export * from "./model/products-services"
+export * from "./model/index"
 export * from "./util/message-service"
