1644c26bef60a27ac6f886988826015d18509d14
bloo <nexushellstein@gmail.com>
2025-11-23 17:17:54 +0800

    minor changes

--- backend/app/controllers/admin_controller.py
+++ backend/app/controllers/admin_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 from ..services.admin_service import AdminService
 
 admin_bp = Blueprint("admin_bp", __name__)
@@ -8,8 +8,12 @@ admin_bp = Blueprint("admin_bp", __name__)
 @jwt_required()
 def inventory():
 	user = get_jwt_identity()
-	if user["role"] != "business_owner":
+	claims = get_jwt()
+
+	# make into util functions
+	if claims.get("role") != "admin":
 		return jsonify({"error": "Unauthorized"}), 403
+
 	products = AdminService.get_inventory()
 	return jsonify(products), 200
 
