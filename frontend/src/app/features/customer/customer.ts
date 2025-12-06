9ac40919a3653f6f586d65895d59a6e6e45011b9
bloo <nexushellstein@gmail.com>
2025-12-07 01:53:53 +0800

    Update customer.ts

--- frontend/src/app/features/customer/customer.ts
+++ frontend/src/app/features/customer/customer.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Shop } from './shop/shop';
 import { ComponentsModule } from '../../shared/components/components-module';
+import { AuthServices } from '../../core/services';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer',
@@ -10,5 +13,17 @@ import { ComponentsModule } from '../../shared/components/components-module';
   styleUrl: './customer.scss',
 })
 export class Customer {
-
+  constructor(
+    private authService: AuthServices,
+    private router: Router
+  ){}
+  logout() {
+    this.authService.logout();
+    Swal.fire({
+      title: "Success!",
+      text: "User Logged Out!",
+      icon: "success"
+    });
+    this.router.navigate(['/auth']);
+  }
 }
