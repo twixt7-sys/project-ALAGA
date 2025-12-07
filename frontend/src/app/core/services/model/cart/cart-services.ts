c9d0edfd078b28316c111d42bd80cc7290ea872b
bloo <nexushellstein@gmail.com>
2025-12-07 18:54:34 +0800

    established and tested cart services

--- frontend/src/app/core/services/model/cart/cart-services.ts
+++ frontend/src/app/core/services/model/cart/cart-services.ts
@@ -1,8 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartServices {
-  
+  private base = 'http://localhost:5000/api/cart';
+  constructor(private http: HttpClient) {}
+
+  addItem(data: any): Observable<any> {
+    return this.http.post(`${this.base}/items`, data);
+  }
 }
