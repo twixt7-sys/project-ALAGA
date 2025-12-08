2a86e64809305d153ff379d2981a6faa16787e63
bloo <nexushellstein@gmail.com>
2025-12-08 20:37:45 +0800

    developed check service

--- frontend/src/app/core/services/util/check-service.ts
+++ frontend/src/app/core/services/util/check-service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckService {
-  
+  user: any = JSON.parse(localStorage.getItem('user') || '{}');
+  checkUser(): boolean{
+    if (!this.user?.user_id) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: `User not found :(`,
+      });
+      return true;
+    }
+    return false;
+  }
 }
